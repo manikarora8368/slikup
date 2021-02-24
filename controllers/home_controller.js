10ad98a22edec69ccd5070e334a66830865e315f
Manik Arora <manikarora8368@gmail.com>
2021-02-24 12:40:55 +0530

    showing posts and user information on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,8 +2,20 @@ const posts = require('../models/post');
 module.exports.home = function(req,res){
     // console.log(req.cookies);
     // res.cookie('user_id' , 25);
-
-    return res.render('home' , {
-        title : "Slikup"
+    // posts.find({},function(err , post){
+    //     if(err){console.log('error in finding the posts');return;}
+    //     return res.render('home' , {
+    //     title : "Slikup",
+    //     Posts: post
+    // })
+    // });
+// populate the user for each post
+    posts.find({}).populate('user').exec(function(err , post){
+        if(err){console.log('error in finding the posts');return;}
+        return res.render('home' , {
+        title : "Slikup",
+        Posts: post
     })
+    });
+
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 const postController = require('../controllers/posts_controller');
 
-router.post('/create_post', postController.createPost);
+router.post('/create_post',passport.checkAuthentication, postController.createPost);
 
 module.exports = router;
